7a81f9e4d1c6fc1ec41b34cf7702b14133cd2081
Uniplac <github@uniplaclages.edu.br>
2023-08-04 16:33:44 -0300

    etapa 3 concluída

--- 3-js/README.md
+++ 3-js/README.md
@@ -7,9 +7,9 @@ Neste teste iremos avaliar o seu conhecimento em JS e organização de código.
 No arquivo **index.html** temos a estrutura de uma página com um código não semântico, mas funcional. Ele já está com o CSS formatado para a exibição da página e de todos os comportamentos que precisam ser aplicados. No HTML você encontrará comentários sobre as classes que ativam as áreas que precisam de interação.
 
 O que precisa ser feito:
-* Abrir e fechar o menu com o clique no botão Menu
-* Play do vídeo ao clicar na imagem de capa
-* Sistema de sanfona para a lista
+* Abrir e fechar o menu com o clique no botão Menu - Okay
+* Play do vídeo ao clicar na imagem de capa - Okay
+* Sistema de sanfona para a lista - Okay
 * Carregamento de conteúdo ajax pela api da [Wikipedia](https://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Alber%20Einstein) (O conteúdo a ser carregado está dentro de query.pages[736].extract)
 * Abrir modal com a foto e fazer função no botão de fechar
 
--- 3-js/assets/js/main.js
+++ 3-js/assets/js/main.js
@@ -1,10 +1,29 @@
 // jQuery
 // $(document).ready(function() {
-  // code
+// code
 // });
 
 // Vanilla JS
 
-// window.onload = function() {
-  // code
-// };
+window.onload = function () {
+  const url =
+    "http://en.wikipedia.org/w/api.php?format=json&action=query&prop=extracts&exintro&explaintext&redirects=1&titles=Alber%20Einstein";
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // Acessar o conteúdo desejado
+      const extract = data.query.pages[736].extract;
+
+      // Exibir no console o conteúdo
+      console.log(extract);
+    })
+    .catch((error) => {
+      console.error("Erro ao carregar conteúdo da Wikipedia:", error);
+    });
+};
+
+document.addEventListener("DOMContentLoaded", window.onload);
+
+// No meu console aparece a mensagem de erro e também assim: from origin 'http://127.0.0.1:5500' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled. Confesso que estudei bastante o erro de CORS mas por não ser tão rotineiro não consegui encontrar uma solução a tempo.
